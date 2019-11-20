580880905bd670e65da0a1b535195d829be8e536
oceaquaris <oceaquarisoxyheloris@gmail.com>
2019-11-19 21:21:25 -0500

    remove extra function, standardize api for use in optimization algos

--- pybropt/objfn/opv.py
+++ pybropt/objfn/opv.py
@@ -1,8 +1,8 @@
 import numpy
 
-# TODO: clean me!
-
-# good function: can be used with optimzation algorithms
+################################################################################
+# Calculate OPV using a haplotype value matrix.
+################################################################################
 def opv(rslice, hcoeff):
     """
     Score a population of individuals based on Optimal Population Value (OPV)
@@ -19,6 +19,11 @@ def opv(rslice, hcoeff):
 
     Parameters
     ----------
+    rslice : numpy.ndarray, tuple, list, None
+        A 1D array of indices to use for slicing and summing the matrix by row.
+        Each index in 'rslice' represents a single individual's row. If 'rslice'
+        is None, use all individuals.
+        # TODO: performance testing to see which array type is better.
     hcoeff : numpy.ndarray
         An array of coefficients for haplotype effects. The dtype of 'hcoeff'
         should be either 'float32' or 'float64'. Array shape should be
@@ -27,11 +32,6 @@ def opv(rslice, hcoeff):
         number of haplotypes. Array format should be the 'C' format.
         # TODO: performance testing to see which array format is better 'F' or
                 'C'.
-    rslice : numpy.ndarray, tuple, list, None
-        A 1D array of indices to use for slicing and summing the matrix by row.
-        Each index in 'rslice' represents a single individual's row. If 'rslice'
-        is None, use all individuals.
-        # TODO: performance testing to see which array type is better.
 
     Returns
     -------
@@ -53,70 +53,10 @@ def opv(rslice, hcoeff):
     ).sum()                 # take sum of maximum haplotypes
 
 
-################################################################################
-# Calculate OPV using a haplotype value matrix.
-################################################################################
-def opv_hcoeff(hcoeff, rslice = None):
-    """
-    Score a population of individuals based on Optimal Population Value (OPV)
-    (Goiffon et al., 2017). Scoring for OPV is defined as the Genomic Estimated
-    Breeding Value (GEBV) of the best inbred progeny produced from a breeding
-    population allowing for an infinite number of intercrossings and
-    generations. Haplotypes can be specified in OPV and markers within the
-    haplotypes are assumed to always segregate with the haplotype. In simpler
-    terms, OPV collects all of the best haplotype blocks and calculates the GEBV
-    for those haplotypes for a given set of individuals.
-
-    OPV selects the 'q' individuals to maximize the maximum GEBV possible within
-    a population consisting of the 'q' selected individuals.
-
-    Parameters
-    ----------
-    hcoeff : numpy.ndarray
-        An array of coefficients for haplotype effects. The dtype of 'hcoeff'
-        should be either 'float32' or 'float64'. Array shape should be
-        (depth, row, column) = (M, N, H) where 'M' represents number of
-        chromosome phases, 'N' represents number of individuals, 'H' represents
-        number of haplotypes. Array format should be the 'C' format.
-        # TODO: performance testing to see which array format is better 'F' or
-                'C'.
-    rslice : numpy.ndarray, tuple, list, None
-        A 1D array of indices to use for slicing and summing the matrix by row.
-        Each index in 'rslice' represents a single individual's row. If 'rslice'
-        is None, use all individuals.
-        # TODO: performance testing to see which array type is better.
-
-    Returns
-    -------
-    float
-        Returns a floating point number representing the OPV score. Data type
-        depends on the dtype of 'coeff'.
-    """
-    ############################################################################
-    # Algorithm:
-    #     Take the array max (numpy.amax) along the chromosome and individual
-    #     axes (axis=(0,1), respectively), then take the sum of the 1D array to
-    #     arrive at OPV GEBV.
-    ############################################################################
-
-    # if we only want specific rows, subset those rows, then calculate sum.
-    if rslice != None:
-        return numpy.amax(      # array max function
-            hcoeff[:,rslice,:], # subset hcoeff rows
-            axis=(0,1)          # calculate max along slice and row axes
-                                # NOTE: this is on a per-haplotype basis.
-        ).sum()                 # take sum of maximum haplotypes
-
-    # otherwise, we don't care and consider all individuals
-    return numpy.amax(          # array max function
-        hcoeff,                 # all hcoeff rows
-        axis=(0,1)              # calculate max along slice and row axes
-                                # NOTE: this is on a per-haplotype basis.
-    ).sum()                     # take sum of maximum haplotypes
 
 ################################################################################
 # Calculate OPV using genotype, coefficient, hbin matrices.
-# I will probably depricate this.
+# This is depricated.
 ################################################################################
 def opv_geno(geno, coeff, hbin = None, rslice = None):
     """
