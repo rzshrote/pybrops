9fb282912ae69704fef8bf5cc7b4917f2e1dbdc7
oceaquaris <oceaquarisoxyheloris@gmail.com>
2019-11-19 21:20:37 -0500

    alter cgs api to be more standardized

--- pybropt/objfn/cgs.py
+++ pybropt/objfn/cgs.py
@@ -1,6 +1,6 @@
 import numpy
 
-def cgs(geno, coeff, rslice = None):
+def cgs(rslice, geno, coeff):
     """
     Score a population of individuals based on Conventional Genomic Selection
     (CGS) (Meuwissen et al., 2001). Scoring for CGS is defined as the sum of
@@ -10,6 +10,11 @@ def cgs(geno, coeff, rslice = None):
 
     Parameters
     ----------
+    rslice : numpy.ndarray, tuple, list, None
+        A 1D array of indices to use for slicing and summing the matrix by row.
+        Each index in 'rslice' represents a single individual's row. If 'rslice'
+        is None, use all individuals.
+        # TODO: performance testing to see which array type is better.
     geno : numpy.ndarray
         An array of allele states. The dtype of 'geno' should be 'uint8'. Array
         shape should be (depth, row, column) = (M, N, L) where 'M' represents
@@ -21,11 +26,6 @@ def cgs(geno, coeff, rslice = None):
         An array of coefficients for allele effects. The dtype of 'coeff'
         should be either 'float32' or 'float64'. This array should be single
         dimensional (column,) = (N,) where 'N' represents number of markers.
-    rslice : numpy.ndarray, tuple, list, None
-        A 1D array of indices to use for slicing and summing the matrix by row.
-        Each index in 'rslice' represents a single individual's row. If 'rslice'
-        is None, use all individuals.
-        # TODO: performance testing to see which array type is better.
 
     Returns
     -------
@@ -33,14 +33,8 @@ def cgs(geno, coeff, rslice = None):
         Returns a 1D array of floating point number representing GEBVs for each
         individual.
     """
-    if rslice != None:
-        return numpy.dot(       # take dot product of each row in each slice
-            geno[:,rslice,:],   # select rows of the matrix
-            coeff               # multiply by marker coefficients
-        ).sum(0)                # Sum across axis=0 to combine dots products
-                                # between slices.
-    return numpy.dot(           # take dot product of each row in each slice
-        geno,                   # select rows of the matrix
-        coeff                   # multiply by marker coefficients
-    ).sum(0)                    # Sum across axis=0 to combine dots products
-                                # between slices.
+    return numpy.dot(       # take dot product of each row in each slice
+        geno[:,rslice,:],   # select rows of the matrix
+        coeff               # multiply by marker coefficients
+    ).sum(0)                # Sum across axis=0 to combine dots products
+                            # between slices.
