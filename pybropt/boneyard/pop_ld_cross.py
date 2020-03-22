16ceac41bcdf54bb890803a491d64dc6030752f6
oceaquaris <oceaquarisoxyheloris@gmail.com>
2020-03-22 11:57:48 -0400

    remove stuff from boneyard

--- pybropt/boneyard/pop_ld_cross.py
+++
@@ -1,5 +0,0 @@
-import numpy
-
-def pop_ld_cross(pRec, mpAB, fpAB, mfreq, ffreq):
-    return ((0.5 * (1 - pRec) * (mpAB + fpAB)) +
-            (0.5 * pRec * ((mfreq[:,None] * ffreq) + (ffreq[:,None] * mfreq))))
