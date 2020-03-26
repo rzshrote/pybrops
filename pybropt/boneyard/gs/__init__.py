ca0e4f0b4a550f169af7b2384011f496a804320b
oceaquaris <oceaquarisoxyheloris@gmail.com>
2020-03-26 14:42:15 -0400

    remove stuff from boneyard

--- pybropt/boneyard/gs/__init__.py
+++
@@ -1,7 +0,0 @@
-# import all files
-from .cgs import cgs, cgs_sim
-from .opv import opv, opv_sim
-from .wgs import wgs, wgs_sim
-from .pa import pa, pa_sim
-from .pa_v2 import pa_v2, pa_v2_sim
-from .mogs import mogs, mogs_sim
