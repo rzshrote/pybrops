d83dab08f5eedc10dbb78e4ae1aaa1569c4a1de2
oceaquaris <oceaquarisoxyheloris@gmail.com>
2020-03-31 13:55:42 -0400

    start test cases

---
+++ pybropt/test/algo/test_StateHC.py
@@ -0,0 +1,10 @@
+# append paths
+import sys
+import os
+sys.path.append(os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))))
+
+# import 3rd party modules we'll need
+import unittest
+
+# import our libraries
+import pybropt.algo.StateHC
