2408e7f8efadb5f6b6733f67324dffec72307f08
oceaquaris <rzshrote@gmail.com>
2021-01-02 18:22:22 -0500

    improve imports

--- pybropt/model/vmat/__init__.py
+++ pybropt/model/vmat/__init__.py
@@ -1,1 +1,23 @@
+# order dependent imports
+
+# level 0 utilities
 from .vmat_util import *
+
+# level 1 interface
+from .VarianceMatrix import *
+
+# level 2 interface
+from .GeneticVarianceMatrix import *
+from .GenicVarianceMatrix import *
+
+# level 3 interface
+from .AdditiveGeneticVarianceMatrix import *
+from .AdditiveGenicVarianceMatrix import *
+
+# level 1 implementation
+from .TwoWayAdditiveGeneticVarianceMatrix import *
+from .ThreeWayAdditiveGeneticVarianceMatrix import *
+from .FourWayAdditiveGeneticVarianceMatrix import *
+from .DihybridAdditiveGeneticVarianceMatrix import *
+
+from .TwoWayAdditiveGenicVarianceMatrix import *
