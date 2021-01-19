95320db0f20d80bd1e28c51aef05c8f441b95855
oceaquaris <rzshrote@gmail.com>
2021-01-19 12:51:20 -0500

    remove old files

--- experiments/2020-Jan-08_exp02/2020-Jan-08_pldd_topology_analysis.R
+++
@@ -1,9 +0,0 @@
-#set working directory
-setwd("C:/Users/rs14/Documents/research/breeding_optimization/PyBrOpt/experiments/2020-Jan-08_exp02/")
-
-# load data frame
-pts_df <- read.csv("pldd_topology.tsv", sep='\t', header=T)
-model1 <- lm(paa ~ pldd, data=pts_df)
-
-with(pts_df, plot(paa, pldd, xlab="PAA", ylab="PLDD", main="PAA vs PLDD", pch=3))
-abline(model1, col="red", lwd=2)
