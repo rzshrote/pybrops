001ea40dfd2beb71a7e02e3141f5dc68f0318b84
oceaquaris <rzshrote@gmail.com>
2022-01-09 14:24:47 -0500

    change op.ssel import structure

--- pybropt/test/breed/op/ssel/SurvivorSelectionOperator_test.py
+++ pybropt/test/breed/op/ssel/SurvivorSelectionOperator_test.py
@@ -9,10 +9,10 @@ from pybropt.test import generic_assert_abstract_property
 from pybropt.test import generic_assert_concrete_method
 from pybropt.test import generic_assert_concrete_function
 
-from pybropt.breed.op.ssel import SurvivorSelectionOperator
-from pybropt.breed.op.ssel import is_SurvivorSelectionOperator
-from pybropt.breed.op.ssel import check_is_SurvivorSelectionOperator
-from pybropt.breed.op.ssel import cond_check_is_SurvivorSelectionOperator
+from pybropt.breed.op.ssel.SurvivorSelectionOperator import SurvivorSelectionOperator
+from pybropt.breed.op.ssel.SurvivorSelectionOperator import is_SurvivorSelectionOperator
+from pybropt.breed.op.ssel.SurvivorSelectionOperator import check_is_SurvivorSelectionOperator
+from pybropt.breed.op.ssel.SurvivorSelectionOperator import cond_check_is_SurvivorSelectionOperator
 
 ################################################################################
 ################################ Test fixtures #################################
