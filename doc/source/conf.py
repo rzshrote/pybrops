ba6daae8d8e7dc4f8c97ddfd6d5090e0dab32d51
oceaquaris <rzshrote@gmail.com>
2023-09-12 15:43:59 -0400

    clean up config files; remove old, unneeded files

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -25,7 +25,7 @@ copyright = '2023, Robert Shrote'
 author = 'Robert Shrote'
 
 # The full version, including alpha/beta/rc tags
-release = '0.0.1'
+release = '1.0.0'
 
 
 # -- General configuration ---------------------------------------------------
